aae92cf4cf9672c590d64cd4262c6d77e486b04e
drvg5 <32469979+drvg5@users.noreply.github.com>
2025-10-22 17:22:21 +0530

    Documenting REST api usingOpenApi
    Documenting REST api usingOpenApi

--- section2/accounts/src/main/java/com/microstart/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/microstart/accounts/AccountsApplication.java
@@ -1,11 +1,39 @@
 package com.microstart.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+//in case the packages are not scanned properly, use the below annotations,
+// for e.g. when folder structure is different than the one we are using
+/*@ComponentScans({ @ComponentScan("com.micro.accounts.controller") })
+@EnableJpaRepositories("com.micro.accounts.repository")
+@EntityScan("com.micro.accounts.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Bank Accounts microservice REST API Documentation using OpenAPI",
+				version = "v1",
+				contact = @Contact(
+						name = "Dhruv Gusain",
+						email = "dorove55@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Bank Accounts microservice REST API Documentation",
+				url = "http://localhost:8080/swagger-ui/index.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
