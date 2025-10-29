19afb923491924da37e52e793dd8301ad646fd66
drvg5 <32469979+drvg5@users.noreply.github.com>
2025-10-29 19:26:13 +0530

    Loans Micro-service
    Push in sll files for the new Loans Micro-service

---
+++ section2/cards/src/test/java/com/microstart/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microstart.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
